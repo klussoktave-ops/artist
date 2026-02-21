a6e3c84ee6bc21c373d3e5fa7eec0bd8332bebb5
klussoktave-ops <klussotave@gmail.com>
2026-02-21 13:21:08 +0300

    prototype

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
